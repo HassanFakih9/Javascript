77dcded91b315c4a85faf38084a3309ffa51303c
Hassan Fakih <hassan.fakih.80@gmail.com>
2023-07-25 15:54:46 +0300

    Done task 3

---
+++ task3/script.js
@@ -0,0 +1,15 @@
+const num1=30;
+var num2=40;
+if (num1>num2) console.log("Hello");
+else console.log("Bye");
+var canpass=false;
+var num2=20;
+if (canpass)
+{
+    if (num1>num2) console.log("Hello");
+    else console.log("Bye");
+}
+else
+{
+  console.log("Try again");  
+}
