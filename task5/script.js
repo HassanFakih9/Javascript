4ec04a36b89b5bc381cf6e7df6c0d26b514820a9
Hassan Fakih <hassan.fakih.80@gmail.com>
2023-07-26 12:46:41 +0300

    task5 done

---
+++ task5/script.js
@@ -0,0 +1,5 @@
+let myarray = [4, 7, 8, 12, 20, 23, 26, 20,25];
+for (let i = 0; i < myarray.length; i++) {
+    if ((myarray[i] % 2) == 0) { console.log(myarray[i], ' is even'); }
+    else { console.log(myarray[i], ' is odd'); }
+}
