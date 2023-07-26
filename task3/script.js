8d5b3bd552b18c4a62ff1d0b61c44edc68120a61
Hassan Fakih <hassan.fakih.80@gmail.com>
2023-07-26 11:31:17 +0300

    task 4 done

--- task3/script.js
+++ task3/script.js
@@ -1,15 +1,11 @@
-const num1=30;
-var num2=40;
-if (num1>num2) console.log("Hello");
+const num1 = 30;
+var num2 = 40;
+if (num1 > num2) console.log("Hello");
 else console.log("Bye");
-var canpass=false;
-var num2=20;
-if (canpass)
-{
-    if (num1>num2) console.log("Hello");
+var canpass = false;
+var num2 = 20;
+if (canpass) {
+    if (num1 > num2) console.log("Hello");
     else console.log("Bye");
 }
-else
-{
-  console.log("Try again");  
-}
+    console.log("Try again");
---
+++ task4/script.js
@@ -0,0 +1,8 @@
+let array1=[1,2,3,4,5,6];
+array2=[];
+array3= Array();
+array2=["hassan","fakih","bootcamp","simplon","auf"];
+array3=['c',2,5,'b',"hello"]
+console.log('array1 ',array1)
+console.log('array2 ',array2)
+console.log('array3 ',array3)
